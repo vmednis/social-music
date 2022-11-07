bcbbb59315886d27ed2574d982c7e24965cdc927
Valters Mednis <valters.mednis@gmail.com>
2022-11-08 01:08:09 +0200

    Routes depend on logged in status

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,29 +1,42 @@
 <script>
   import {Router, Link, Route} from "svelte-navigator";
   import RoomPreJoin from "/src/pages/RoomPreJoin.svelte";
-  import NewRoom from "/src/pages/NewRoom.svelte";
+  import RoomNew from "/src/pages/RoomNew.svelte";
+
+  let is_logged_in = false;
+  if(document.cookie.match(/^(.*;)?\s*userid\s*=\s*[^;]+(.*)?$/)) {
+    is_logged_in = true;
+  }
 
   let greeting = "No greeting :(";
-  fetch("/test").then((response) => response.text()).then((body) => greeting = body);
+  if(is_logged_in) {
+    fetch("/test").then((response) => response.text()).then((body) => greeting = body);
+  }
 </script>
 
 <Router>
+{#if is_logged_in}
   <div class="h-screen max-h-screen w-screen flex flex-col overflow-hidden">
     <header class="bg-zinc-700 text-white w-screen p-2 grow-0">
       <ul class="flex flex-row">
         <li><Link to="" class="p-2 hover:bg-zinc-500">Home</Link></li>
         <li><Link to="/room/new" class="p-2 hover:bg-zinc-500">New Room</Link></li>
-        <li><a href="/login" class="p-2 hover:bg-zinc-500">Login with Spotify</a></li>
       </ul>
     </header>
     <main class="flex-auto min-h-0">
       <Route path="">
         <p>{greeting}</p>
       </Route>
-      <Route path="room/new" component={NewRoom}/>
+      <Route path="room/new" component={RoomNew}/>
       <Route path="room/:roomId" component={RoomPreJoin}/>
     </main>
   </div>
+{:else}
+  <Route path="">
+    <a href="/login">Login with Spotify</a>
+  </Route>
+{/if}
+
 </Router>
 
 <style>
