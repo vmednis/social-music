f46cec34e4d3b578772116206076576c20982001
Valters Mednis <valters.mednis@gmail.com>
2023-01-06 03:36:43 +0200

    Join queue button

--- client/src/pages/room/RoomQueue.svelte
+++ client/src/pages/room/RoomQueue.svelte
@@ -1,8 +1,13 @@
 <script>
   import { socket } from "./socket.js";
+
+  function onJoin() {
+    socket.sendJoinQueue();
+  }
 </script>
 
 <div>
+  <button on:click={onJoin} class="p-1 w-full border-amber-500 border rounded bg-amber-400 hover:bg-amber-500 active:bg-amber-600">Join Queue</button>
   <ol>
     {#each $socket.queue as user}
       <li>{user}</li>
